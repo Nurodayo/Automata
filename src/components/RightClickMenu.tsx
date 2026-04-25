7e7d926fbc015d09769361c7f493d4fc69f2a2e9
Nurodayo <cianuroicecream@gmail.com>
2026-04-25 18:42:48 -0400

    Further developments into Right click menu style

--- src/components/RightClickMenu.tsx
+++ src/components/RightClickMenu.tsx
@@ -8,21 +8,27 @@ type MenuOptions = {
 type MenuProps = {
   options: MenuOptions[];
 };
-
+// i hate styling
 // We pass an object in Canvas.tsx which is going to have a label for the button and the function that it will on click
 // this file is going to be mostly style
 function RightClickMenu({ options }: MenuProps) {
   return (
-    <div className="flex flex-col items-start">
-      <div className="flex flex-row padding-8">
-        <RiFunctionFill />
-        <p>Menu</p>
+    <div className="flex flex-col border-1 border-black/50 w-64 rounded-md">
+      <div className="flex flex-row p-2">
+        <RiFunctionFill size={32} />
+        <p className="text-2xl font-medium">Menu</p>
       </div>
-      {options.map((e) => (
-        <button key={e.id} onClick={e.method}>
-          {e.label}
-        </button>
-      ))}
+      <div className="flex flex-col items-center pb-1">
+        {options.map((e) => (
+          <button
+            key={e.id}
+            onClick={e.method}
+            className="p-1 w-62 hover:bg-gray-200 rounded-md align-start text-lg justify-center border-b-1 border-transparent hover:border-gray-500  duration-200 ease-in-out"
+          >
+            {e.label}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
