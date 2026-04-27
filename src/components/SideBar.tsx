e95a647f5277974eba996d36a61f006dcd33a5b8
Nurodayo <cianuroicecream@gmail.com>
2026-04-27 16:55:40 -0400

    further developments on sidebar again

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,7 +4,7 @@
 function SideBar() {
   return (
     <div className="flex flex-col w-[20vw] border-r-1 border-black/50">
-      <div className="w-full mr-auto ml-auto p-2">
+      <div className="w-full mr-auto ml-auto p-2 border-b border-black/50">
         <div className="flex gap-1 w-full">
           <button className="flex-1 truncate text-lg border border-black/50 rounded-full py-1">
             State
@@ -14,13 +14,22 @@ function SideBar() {
           </button>
         </div>
       </div>
+      {/* We need to add some logic to when we chose the settings tab*/}
       <div className="w-full mr-auto ml-auto p-2">
         <div className="flex gap-1 w-full">
           <p className="flex-1 truncate text-xl text-center rounded-full font-medium py-1">
-            Current State
+            Current State.
           </p>
-          <button className="flex-1 truncate text-lg border border-black/50 rounded-full py-1">
-            Settings
+          <button className=" font-[JetBrains_Mono] truncate text-xl border border-black/50 rounded-full w-[5vw] py-1">
+            q0
+          </button>
+        </div>
+        <div className="flex gap-1 w-full py-2">
+          <p className="flex-1 truncate text-xl text-center rounded-full font-medium py-1">
+            Terminal State.
+          </p>
+          <button className="w-[5vw] font-[JetBrains_Mono] truncate text-xl border border-black/50 rounded-full py-1">
+            q0
           </button>
         </div>
       </div>
